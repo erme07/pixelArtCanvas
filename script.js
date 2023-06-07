c968dca2b7036ed052822fe43a0233c0ef4e6fb9
erme07 <erik.medina07@gmail.com>
2023-06-07 18:15:41 -0300

    update

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const lienzo = document.querySelector(".canvas")
 let grilla = document.createDocumentFragment();
 for (i = 0; i < 980; i++) {
   const celda = document.createElement("DIV");
-  celda.classList.add("celda")
+  celda.classList.add("cell")
+  celda.id = `cell${i + 1}`
   grilla.appendChild(celda)
 }
 lienzo.appendChild(grilla);
