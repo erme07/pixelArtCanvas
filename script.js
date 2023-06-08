41292e6c77cb83272400b3bffc94c0b8eb1c8252
erme07 <erik.medina07@gmail.com>
2023-06-08 19:36:52 -0300

    update

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-const lienzo = document.querySelector(".canvas")
-const menu = document.querySelector(".menu")
-const picker = document.querySelector(".picker")
+const lienzo = document.querySelector(".canvas"),
+  menu = document.querySelector(".menu"),
+  picker = document.querySelector(".picker");
 
-let grilla = document.createDocumentFragment();
-for (i = 0; i < 1064; i++) {
-  const celda = document.createElement("DIV");
-  celda.classList.add("cell")
-  celda.id = `cell${i + 1}`
-  grilla.appendChild(celda)
-}
-lienzo.appendChild(grilla);
+let press = false,
+  eraser = false,
+  grilla = document.createDocumentFragment();
 
 const draw = () => {
   menu.children[2].classList.remove("active");
   menu.children[1].classList.add("active");
   lienzo.classList.add("cursorPen");
   lienzo.classList.remove("cursorEraser")
+  eraser = false;
 }
 
 const erase = () => {
@@ -23,27 +19,51 @@ const erase = () => {
   menu.children[2].classList.add("active");
   lienzo.classList.remove("cursorPen");
   lienzo.classList.add("cursorEraser")
+  eraser = true
 }
 
 const clear = () => {
   draw()
+  lienzo.childNodes.forEach((e) => {
+    e.style.backgroundColor = "transparent";
+  })
   picker.value = "#000000"
+  eraser = false;
 }
 
+for (i = 0; i < 1064; i++) {
+  const celda = document.createElement("DIV");
+  celda.classList.add("cell")
+  celda.id = `cell${i + 1}`
+  grilla.appendChild(celda)
+}
+
+lienzo.appendChild(grilla);
+
 menu.addEventListener("click", (e) => {
-  if (e.target.getAttribute("name") === "draw") {
+  if (e.target.getAttribute("name") === "draw")
     draw();
-  }
-  else if (e.target.getAttribute("name") === "erase") {
+  else if (e.target.getAttribute("name") === "erase")
     erase();
-  }
-  else if (e.target.getAttribute("name") === "clear") {
+  else if (e.target.getAttribute("name") === "clear")
     clear();
-  }
 })
 
-lienzo.addEventListener("click", (e) => {
-  if (e.target.getAttribute("class") === "cell") {
-    e.target.style.backgroundColor = picker.value;
-  }
+lienzo.addEventListener("mousedown", (e) => {
+  press = true;
+  if (!eraser)
+    e.target.style.backgroundColor = picker.value
+  else
+    e.target.style.backgroundColor = "transparent"
+})
+
+lienzo.addEventListener("mouseup", (e) => {
+  press = false;
+})
+
+lienzo.addEventListener("mousemove", (e) => {
+  if (press && !eraser)
+    e.target.style.backgroundColor = picker.value
+  else if (press && eraser)
+    e.target.style.backgroundColor = "transparent"
 })
