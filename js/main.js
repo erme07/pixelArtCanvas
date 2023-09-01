65f8365e9a3d5070370976b382d0ef1bd8e28bef
erme07 <erik.medina07@gmail.com>
2023-09-01 10:38:04 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
+
 const $lienzo = document.querySelector(".canvas"),
-  $menu = document.querySelector(".menu"),
   $buttonDraw = document.querySelector("[data-action='draw']"),
   $buttonEraser = document.querySelector("[data-action='erase']"),
   $buttonGrid = document.querySelector("[data-action='grid']"),
-  $picker = document.querySelector(".picker"),
   $pickerColor = document.querySelector(".picker__color"),
-  $body = document.querySelector("body"),
-  events = {
-    mouse: "mousemove",
-    touch: "touchmove"
-  };
-let press = false,
-  eraser = false,
-  grilla = document.createDocumentFragment(),
+  $cleanConfirm = document.getElementById('cleanConfirm'),
+  $body = document.querySelector("body");
+
+let grid = document.createDocumentFragment(),
   deviceType = "",
-  numCeldas = "",
-  pickerColor = "",
   cursorPen = "";
 
-let currentColor = '#000000';
+const $colorPicker = document.querySelector('.color-picker'),
 
-let hue = 0;
-let hsvSaturation = 1;
-let hsvValue = 1;
+  $spectrumCanvas = document.getElementById('spectrum-canvas'),
+  $spectrumMap = document.getElementById('spectrum-map'),
+  spectrumCtx = $spectrumCanvas.getContext('2d'),
+  $spectrumCursor = document.getElementById('spectrum-cursor'),
+
+  $hueCanvas = document.getElementById('hue-canvas'),
+  $hueMap = document.getElementById("hue-map"),
+  hueCtx = $hueCanvas.getContext('2d'),
+  $hueCursor = document.getElementById('hue-cursor'),
+
+  $hex = document.getElementById('hex');
+
+let hueRect = $hueCanvas.getBoundingClientRect(),
+  spectrumRect = $spectrumCanvas.getBoundingClientRect(),
+  hue = 0,
+  hsvSaturation = 1,
+  hsvValue = 1,
+  eraser = false,
+  currentColor = '#000000';
 
 const createPen = () => {
   cursorPen = `<svg width="18" height="18" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="m 12.1,0.146 a 0.5,0.5 0 0 1 0.8,0 l 3,3.004 a 0.5,0.5 0 0 1 0,0.7 L 5.85,13.9 A 0.5,0.5 0 0 1 5.69,14 l -5.004,2 a 0.5,0.5 0 0 1 -0.65,-0.7 l 2.004,-5 a 0.5,0.5 0 0 1 0.11,-0.2 z" /><path d="M 11.2,2.5 13.5,4.79 14.8,3.5 12.5,1.21 Z" fill="#ffffff" /><path d="M 12.8,5.5 10.5,3.21 4,9.71 V 10 H 4.5 A 0.5,0.5 0 0 1 5,10.5 V 11 H 5.5 A 0.5,0.5 0 0 1 6,11.5 V 12 h 0.29 z" fill="#ffffff" /><path d="M 3.03,10.7 2.93,10.8 1.4,14.6 5.22,13.1 5.33,13 A 0.5,0.5 0 0 1 5,12.5 V 12 H 4.5 A 0.5,0.5 0 0 1 4,11.5 V 11 H 3.5 A 0.5,0.5 0 0 1 3.03,10.7 Z" fill="${currentColor}" /></svg>`;
@@ -43,23 +52,21 @@ const erase = () => {
   eraser = true
 }
 const clear = () => {
-  let confirma = confirm("Do you really want to clean the canvas?");
-  if (!confirma) return;
   draw()
   $lienzo.childNodes.forEach((e) => {
     e.style.backgroundColor = "transparent";
   })
+  $cleanConfirm.classList.toggle('show')
+  document.querySelector('.overlay--confirm').classList.toggle('show');
   eraser = false;
 }
 
 const detectDevice = () => {
   try {
     document.createEvent("TouchEvent");
-    deviceType = "touch";
-    return true;
+    return 'touchmove';
   } catch (e) {
-    deviceType = "mouse";
-    return false;
+    return 'mousemove';
   }
 }
 const detectWidth = () => {
@@ -72,11 +79,11 @@ const detectWidth = () => {
 }
 
 const makeGrid = () => {
-  for (i = 0; i < numCeldas; i++) {
+  for (let i = 0; i < detectWidth(); i++) {
     const celda = document.createElement("DIV");
     celda.classList.add("cell")
     celda.id = `cell${i + 1}`
-    grilla.appendChild(celda)
+    grid.appendChild(celda)
   }
 }
 
@@ -85,103 +92,47 @@ const showHideGrid = () => {
   $lienzo.classList.toggle("noline");
 }
 
-createPen();
-detectDevice();
-numCeldas = detectWidth();
-makeGrid();
-
-$lienzo.appendChild(grilla);
-
-
-
-$lienzo.addEventListener("mousedown", (e) => {
-  press = true;
-  if (!eraser)
-    e.target.style.backgroundColor = currentColor;
-  else
-    e.target.style.backgroundColor = "transparent";
-
-})
-
-document.addEventListener("mouseup", (e) => {
-  press = false;
-})
-
-
-
-$lienzo.addEventListener(events[deviceType], (e) => {
-
-  if (detectDevice()) {
-    e.preventDefault()
-    let elementId = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY).id;
+const startDraw = () => {
+  if (deviceType === 'touchmove') {
+    event.preventDefault()
+    let elementId = document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY).id;
     if (!eraser)
       document.getElementById(elementId).style.backgroundColor = currentColor;
-    else if (eraser)
+    else
       document.getElementById(elementId).style.backgroundColor = "transparent"
   } else {
-    if (press && !eraser && e.target.classList.contains("cell"))
-      e.target.style.backgroundColor = currentColor
-    else if (press && eraser && e.target.classList.contains("cell"))
-      e.target.style.backgroundColor = "transparent"
+    if (!eraser)
+      event.target.style.backgroundColor = currentColor
+    else
+      event.target.style.backgroundColor = "transparent"
   }
-})
-
-document.addEventListener("resize", () => {
-  numCeldas = detectWidth();
-  makeGrid()
-  $lienzo.innerHTML = "";
-  $lienzo.appendChild(grilla);
-})
-
-
-
-
-const colorPicker = document.querySelector('.color-picker');
-
-const spectrumCanvas = document.getElementById('spectrum-canvas');
-const spectrumMap = document.getElementById('spectrum-map');
-const spectrumCtx = spectrumCanvas.getContext('2d');
-const spectrumCursor = document.getElementById('spectrum-cursor');
-let spectrumRect = spectrumCanvas.getBoundingClientRect();
-
-const hueCanvas = document.getElementById('hue-canvas');
-const hueMap = document.getElementById("hue-map");
-const hueCtx = hueCanvas.getContext('2d');
-const hueCursor = document.getElementById('hue-cursor');
-let hueRect = hueCanvas.getBoundingClientRect();
-
-const hex = document.getElementById('hex');
-
-
-
-const refreshElementRects = () => {
-  spectrumRect = spectrumCanvas.getBoundingClientRect();
-  hueRect = hueCanvas.getBoundingClientRect();
 }
 
-const createSpectrum = (color) => {
-  ctx = spectrumCtx;
-  if (!color) color = '#f00';
+/////
 
-  const mainGradient = ctx.createLinearGradient(0, 0, spectrumCanvas.width, 0);
+const refreshElementRects = () => {
+  spectrumRect = $spectrumCanvas.getBoundingClientRect();
+  hueRect = $hueCanvas.getBoundingClientRect();
+}
+
+const createSpectrum = (color = '#ff0000') => {
+  let ctx = spectrumCtx;
+  const mainGradient = ctx.createLinearGradient(0, 0, $spectrumCanvas.width, 0);
   mainGradient.addColorStop(0, "#fff");
   mainGradient.addColorStop(1, color);
-
-  const blackGradient = ctx.createLinearGradient(0, 0, 0, spectrumCanvas.height);
+  const blackGradient = ctx.createLinearGradient(0, 0, 0, $spectrumCanvas.height);
   blackGradient.addColorStop(0, "transparent");
   blackGradient.addColorStop(1, "#000");
-
   ctx.fillStyle = mainGradient;
-  ctx.fillRect(0, 0, spectrumCanvas.width, spectrumCanvas.height);
+  ctx.fillRect(0, 0, $spectrumCanvas.width, $spectrumCanvas.height);
   ctx.fillStyle = blackGradient;
-  ctx.fillRect(0, 0, spectrumCanvas.width, spectrumCanvas.height);
+  ctx.fillRect(0, 0, $spectrumCanvas.width, $spectrumCanvas.height);
 
 };
 
-
 const createHue = () => {
   const ctx = hueCtx;
-  const hueGradient = ctx.createLinearGradient(0, 0, hueCanvas.width, 0);
+  const hueGradient = ctx.createLinearGradient(0, 0, $hueCanvas.width, 0);
   hueGradient.addColorStop(0.00, "#FF0000");
   hueGradient.addColorStop(0.17, "#FA00FF");
   hueGradient.addColorStop(0.33, "#0400FF");
@@ -190,9 +141,26 @@ const createHue = () => {
   hueGradient.addColorStop(0.83, "#FAFF00");
   hueGradient.addColorStop(1.00, "#FF0000");
   ctx.fillStyle = hueGradient;
-  ctx.fillRect(0, 0, hueCanvas.width, hueCanvas.height);
+  ctx.fillRect(0, 0, $hueCanvas.width, $hueCanvas.height);
+}
+
+const updateSpectrumCursor = (x, y) => {
+  $spectrumCursor.style.left = x + 'px';
+  $spectrumCursor.style.top = y + 'px';
 };
 
+const updateHueCursor = (x) => {
+  $hueCursor.style.left = x + "px";
+}
+
+const setCurrentColor = (color) => {
+  currentColor = color;
+  $pickerColor.style.backgroundColor = currentColor;
+}
+
+const setColorValues = (color) => {
+  $hex.value = color;
+}
 
 const colorToPos = (color) => {
   const hsv = tinycolor(color).toHsv();
@@ -201,41 +169,24 @@ const colorToPos = (color) => {
   const y = spectrumRect.height * (1 - hsv.v);
   const hueX = hueRect.width - ((hue / 360) * hueRect.width);
   updateSpectrumCursor(x, y);
-  updateHueCursor(hueX, tinycolor('hsv ' + hue + ' 1 1').toHexString());
+  updateHueCursor(hueX);
   setCurrentColor(color);
   createSpectrum(tinycolor('hsv ' + hue + ' 1 1').toHexString());
-};
-
-const setColorValues = (color) => {
-  hex.value = color;
-};
-
-const setCurrentColor = (color) => {
-  currentColor = color;
-  $pickerColor.style.backgroundColor = color;
-};
-
-const updateHueCursor = (x, color) => {
-  hueCursor.style.left = x + "px";
 }
 
-const updateSpectrumCursor = (x, y) => {
-  spectrumCursor.style.left = x + 'px';
-  spectrumCursor.style.top = y + 'px';
-};
-
-const startGetSpectrumColor = (e) => {
-  $body.classList.add("bodyMove");
-  hueCanvas.classList.add("bodyMove");
-  hueCursor.classList.add("bodyMove");
-  getSpectrumColor(e);
-  document.addEventListener('mousemove', getSpectrumColor);
-  document.addEventListener('mouseup', endGetSpectrumColor);
-};
+const starGetData = (classCursor, action) => {
+  $body.classList.add(classCursor);
+  $spectrumCanvas.classList.add(classCursor);
+  $spectrumCursor.classList.add(classCursor);
+  $hueCanvas.classList.add(classCursor);
+  $hueCursor.classList.add(classCursor);
+  action(event);
+  document.addEventListener('mousemove', action);
+}
 
 const getPositionColor = () => {
-  let cordX = Number(spectrumCursor.style.left.replace("px", ""));
-  let cordY = Number(spectrumCursor.style.top.replace("px", ""));;
+  let cordX = Number($spectrumCursor.style.left.replace("px", ""));
+  let cordY = Number($spectrumCursor.style.top.replace("px", ""));;
   const xRatio = cordX / spectrumRect.width * 100;
   const yRatio = cordY / spectrumRect.height * 100;
   hsvValue = 1 - (yRatio / 100);
@@ -248,29 +199,15 @@ const getSpectrumColor = (e) => {
   refreshElementRects();
   let x = e.pageX - spectrumRect.left;
   let y = e.pageY - spectrumRect.top;
-
   if (x > spectrumRect.width) { x = spectrumRect.width }
   if (x < 0) { x = 0 }
   if (y > spectrumRect.height) { y = spectrumRect.height }
   if (y < 0) { y = 0 }
-
   updateSpectrumCursor(x, y);
   const color = getPositionColor();
   setCurrentColor(color);
   setColorValues(color);
-
 };
-
-function endGetSpectrumColor(e) {
-
-  document.removeEventListener('mousemove', getSpectrumColor);
-  hueCanvas.classList.remove("bodyMove");
-  hueCursor.classList.remove("bodyMove");
-  $body.classList.remove("bodyMove")
-  if (!eraser) createPen();
-};
-
-
 
 const getHueColor = (e) => {
   e.preventDefault();
@@ -280,81 +217,98 @@ const getHueColor = (e) => {
   if (x < 0) x = 0;
   const percent = x / hueRect.width;
   hue = 360 - (360 * percent);
-
   const hueColor = tinycolor('hsv ' + hue + ' 1 1').toHexString();
   const color = getPositionColor();
-
-
   createSpectrum(hueColor);
   updateHueCursor(x, hueColor);
   setCurrentColor(color);
   setColorValues(color);
 };
 
-const endGetHueColor = (e) => {
-  document.removeEventListener('mousemove', getHueColor);
-  spectrumCanvas.classList.remove("bodyEresize");
-  spectrumCursor.classList.remove("bodyEresize");
-  $body.classList.remove("bodyEresize")
-};
 
-const startGetHueColor = (e) => {
-  $body.classList.add("bodyEresize");
-  spectrumCanvas.classList.add("bodyEresize");
-  spectrumCursor.classList.add("bodyEresize");
-  getHueColor(e);
-  document.addEventListener('mousemove', getHueColor);
-  document.addEventListener('mouseup', endGetHueColor);
-}
-
-
+createPen();
+deviceType = detectDevice();
+makeGrid();
+$lienzo.appendChild(grid);
+$colorPicker.style.top = $pickerColor.getBoundingClientRect().bottom + 16 + 'px';
 
 createSpectrum();
 createHue();
 
-spectrumMap.addEventListener('mousedown', function (e) {
-  startGetSpectrumColor(e);
-});
+//////////
 
-hueMap.addEventListener('mousedown', function (e) {
-  startGetHueColor(e);
-});
-
-
-hex.addEventListener("input", (e) => {
-  let go = tinycolor(e.target.value);
-  if (go.isValid()) {
-    e.target.classList.remove("hex--error")
-    e.target.classList.add("hex--succes")
-  }
-  else {
-    e.target.classList.remove("hex--succes")
-    e.target.classList.add("hex--error")
-  }
-  colorToPos(go.toHexString());
+$lienzo.addEventListener("mousedown", (e) => {
+  if (!eraser)
+    e.target.style.backgroundColor = currentColor;
+  else
+    e.target.style.backgroundColor = "transparent";
+  $lienzo.addEventListener(deviceType, startDraw)
 })
 
-colorPicker.style.top = $pickerColor.getBoundingClientRect().bottom + 15 + 'px';
+document.addEventListener("mouseup", (e) => {
+  $lienzo.removeEventListener(deviceType, startDraw);
+  document.removeEventListener(deviceType, getSpectrumColor);
+  document.removeEventListener(deviceType, getHueColor);
+  $body.classList.remove('bodyMove', 'bodyEresize');
+  $spectrumCanvas.classList.remove('bodyMove', 'bodyEresize');
+  $spectrumCursor.classList.remove('bodyMove', 'bodyEresize');
+  $hueCanvas.classList.remove('bodyMove', 'bodyEresize');
+  $hueCursor.classList.remove('bodyMove', 'bodyEresize');
+  if (!eraser) createPen();
+})
 
-
+document.addEventListener("resize", () => {
+  makeGrid()
+  $lienzo.innerHTML = "";
+  $lienzo.appendChild(grid);
+  $colorPicker.style.top = $pickerColor.getBoundingClientRect().bottom + 16 + 'px';
+})
 
 document.addEventListener('click', (e) => {
-
   if (e.target.getAttribute("data-action") === "draw")
     draw();
   else if (e.target.getAttribute("data-action") === "erase")
     erase();
-  else if (e.target.getAttribute("data-action") === "clear")
-    clear();
+  else if (e.target.getAttribute("data-action") === "clear") {
+    $cleanConfirm.classList.toggle('show')
+    document.querySelector('.overlay--confirm').classList.toggle('show');
+  }
   else if (e.target.getAttribute("data-action") === "grid")
     showHideGrid();
-  else if (e.target.getAttribute("data-action") === "copy") {
-    navigator.clipboard.writeText(hex.value);
+  else if (e.target.getAttribute('data-value') === 'accept')
+    clear()
+  else if (e.target.getAttribute('data-value') === 'cancel') {
+    $cleanConfirm.classList.toggle('show')
+    document.querySelector('.overlay--confirm').classList.toggle('show');
   }
-
-  if (e.target.getAttribute('data-action') === 'show-hide') {
-    colorPicker.classList.toggle('show');
+  else if (e.target.getAttribute('data-action') === 'show-hide') {
+    $colorPicker.classList.toggle('show');
     document.querySelector('.overlay').classList.toggle('show');
     document.querySelector('.picker__overlay').classList.toggle('show');
   }
+  else if (e.target.getAttribute("data-action") === "copy")
+    navigator.clipboard.writeText($hex.value);
+})
+
+$spectrumMap.addEventListener('mousedown', (e) => {
+  starGetData('bodyMove', getSpectrumColor)
+});
+
+$hueMap.addEventListener('mousedown', function (e) {
+  starGetData('bodyEresize', getHueColor)
+});
+
+$hex.addEventListener("input", (e) => {
+  let color = tinycolor($hex.value);
+  if (color.isValid()) {
+    // $hex.classList.replace("hex--error", "hex--succes")
+    $hex.classList.remove("hex--error")
+    $hex.classList.add("hex--succes")
+  }
+  else {
+    // $hex.classList.replace("hex--succes", "hex--error")
+    $hex.classList.remove("hex--succes")
+    $hex.classList.add("hex--error")
+  }
+  colorToPos(color.toHexString());
 })
